9e98d6ea944e46507b6b9719eed6d8714aa8b160
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-11 19:27:29 -0300

    problem 3 - valid anagram

---
+++ src/main/java/com/judalabs/leetcode/easy/P3_242.java
@@ -0,0 +1,29 @@
+package com.judalabs.leetcode.easy;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/*
+https://leetcode.com/problems/valid-anagram/
+ */
+public class P3_242 {
+
+    public boolean isAnagram(String s, String t) {
+        Map<Integer, Integer> letterCount = s.chars().boxed()
+                .collect(Collectors.toMap(e -> e, a -> 1, (a, b) -> ++a));
+
+        for(int i = 0; i < t.length(); ++i) {
+            final char ch = t.charAt(i);
+            Integer count = letterCount.get((int) ch);
+            if(count != null) {
+                if(count == 1)
+                    letterCount.remove((int) ch);
+                else
+                    letterCount.put((int) ch, --count);
+            } else {
+                return false;
+            }
+        }
+        return letterCount.isEmpty();
+    }
+}
---
+++ src/test/java/com/judalabs/leetcode/easy/P3_242Test.java
@@ -0,0 +1,28 @@
+package com.judalabs.leetcode.easy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class P3_242Test {
+
+    @ParameterizedTest
+    @MethodSource("examples")
+    void tests(String s, String t, boolean expected) {
+        Assertions.assertEquals(expected, new P3_242().isAnagram(s, t));
+    }
+
+    private static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of("ab", "a", false),
+                Arguments.of("anagram", "nagaram", true),
+                Arguments.of("rat", "car", false),
+                Arguments.of("aba", "baa", true),
+                Arguments.of("tritri", "tirrti", true),
+                Arguments.of("b", "b", true)
+        );
+    }
+}
