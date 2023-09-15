c95f5363e9f0549cc22f4cc80fa2aa070e715ac4
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-15 02:44:35 -0300

    problem  - valid palindrome

---
+++ src/main/java/com/judalabs/leetcode/easy/P5_125.java
@@ -0,0 +1,26 @@
+package com.judalabs.leetcode.easy;
+
+import java.util.Stack;
+
+/*
+https://leetcode.com/problems/valid-palindrome/
+ */
+public class P5_125 {
+
+    public boolean isPalindrome(String s) {
+        final String normalized = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        Stack<Character> stack = new Stack<>();
+
+        int mid = normalized.length() / 2;
+        for(int i = 0; i < mid; i++) {
+            stack.push(normalized.charAt(i));
+        }
+        if(normalized.length() % 2 == 1) mid++;
+
+        for(int i = mid; i < normalized.length(); i++) {
+            if(stack.pop() != normalized.charAt(i)) return false;
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ src/test/java/com/judalabs/leetcode/easy/P5_125Test.java
@@ -0,0 +1,28 @@
+package com.judalabs.leetcode.easy;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class P5_125Test {
+
+    @ParameterizedTest
+    @MethodSource("examples")
+    void test(String s, boolean expected) {
+        Assertions.assertEquals(expected, new P5_125().isPalindrome(s));
+    }
+
+    private static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of("0P", false),
+                Arguments.of("A man, a plan, a canal: Panama", true),
+                Arguments.of("race a car", false),
+                Arguments.of(" ", true),
+                Arguments.of("b a b", true)
+
+        );
+    }
+}
