a6fc8ec79a01c073a741e021e4c43493ae1fb6bd
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-17 21:22:50 -0300

    problem 8 - number of bits 1

---
+++ src/main/java/com/judalabs/leetcode/topswe/P8_191.java
@@ -0,0 +1,22 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.stream.IntStream;
+
+/*
+https://leetcode.com/problems/number-of-1-bits/
+ */
+public class P8_191 {
+
+    public int hammingWeight(int n) {
+        return IntStream.range(0, 32)
+                .map(i -> calculateBitIndex(n, i)).sum();
+    }
+
+    private static int calculateBitIndex(int n, int i) {
+        final int mask = 1 << i;
+        if((n & mask) != 0) {
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/com/judalabs/leetcode/topswe/P8_191Test.java
@@ -0,0 +1,25 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class P8_191Test {
+
+    @ParameterizedTest
+    @MethodSource("examples")
+    void tests(int input, int expected) {
+        Assertions.assertEquals(expected, new P8_191().hammingWeight(input));
+    }
+
+    public static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of(00000000000000000000000000001011, 3),
+                Arguments.of(00000000000000000000000010000000, 1)
+//                Arguments.of(11111111111111111111111111111101, 31)
+        );
+    }
+}
