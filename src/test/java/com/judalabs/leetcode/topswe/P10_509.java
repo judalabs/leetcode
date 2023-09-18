85a48568679055479b81d27504448cdb9ee882d3
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-17 22:51:18 -0300

    problem 10 - fibonnaci

---
+++ src/test/java/com/judalabs/leetcode/topswe/P10_509.java
@@ -0,0 +1,23 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P10_509 {
+
+    private List<Integer> memo = new ArrayList<>();
+
+    public int fib(int n) {
+        if(n <= 2) return 1;
+        memo.add(1);
+        memo.add(1);
+        int valor = 0;
+        int i = 2;
+        while (i < n) {
+            final int fibAtual = memo.get(i - 1) + memo.get(i - 2);
+            memo.add(fibAtual);
+            i++;
+        }
+        return memo.get(memo.size() - 1);
+    }
+}
