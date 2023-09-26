b67882f50205a60cb7667606f6bdeab2590fed2d
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-26 16:16:42 -0300

    problem 12 - min cost climbing stairs

---
+++ src/main/java/com/judalabs/leetcode/topswe/P12_746.java
@@ -0,0 +1,30 @@
+package com.judalabs.leetcode.topswe;
+
+/*
+https://leetcode.com/problems/min-cost-climbing-stairs/
+ */
+public class P12_746 {
+
+
+    public int minCostClimbingStairs(int[] cost) {
+        for(int i = 2; i < cost.length; i++) {
+            cost[i] = Math.min(cost[i-1], cost[i-2]) + cost[i];
+        }
+        return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+    }
+//    public int minCostClimbingStairs(int[] cost) {
+//        return Math.min(
+//                minCostClimbingStairsVisit(cost, cost[0], 0),
+//                minCostClimbingStairsVisit(cost, cost[1], 1));
+//    }
+//
+//    private int minCostClimbingStairsVisit(int[] cost, int actualSum, int index) {
+//        if(index >= cost.length - 2) {
+//            return actualSum;
+//        }
+//        return Math.min(
+//                minCostClimbingStairsVisit(cost, actualSum + cost[index + 1], index + 1),
+//                minCostClimbingStairsVisit(cost, actualSum + cost[index + 2], index + 2)
+//        );
+//    }
+}
---
+++ src/test/java/com/judalabs/leetcode/topswe/P12_746Test.java
@@ -0,0 +1,26 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class P12_746Test {
+
+    @ParameterizedTest
+    @MethodSource("examples")
+    void tests(int[] input, int expected) {
+        Assertions.assertEquals(expected,new P12_746().minCostClimbingStairs(input));
+    }
+
+    public static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of(new int[]{2,2,1,0}, 2),
+                Arguments.of(new int[]{10, 10, 1, 2}, 11),
+                Arguments.of(new int[]{10,15,20}, 15),
+                Arguments.of(new int[]{1,100,1,1,1,100,1,1,100,1}, 6)
+        );
+    }
+}
