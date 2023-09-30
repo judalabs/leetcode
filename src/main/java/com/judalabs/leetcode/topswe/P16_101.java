832f861a669d28362b96361ab92693122306bcac
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-30 20:29:17 -0300

    problem 16 - Symmetric Tree

--- src/main/java/com/judalabs/leetcode/topswe/P16_101.java
+++ src/main/java/com/judalabs/leetcode/topswe/P16_101.java
@@ -8,25 +8,14 @@ https://leetcode.com/problems/symmetric-tree/
 public class P16_101 {
 
     public boolean isSymmetric(TreeNode root) {
-        reverse(root.left);
         return verifyEquals(root.left, root.right);
-    }
-
-    private void reverse(TreeNode actual) {
-        if(actual == null) return;
-
-        TreeNode aux = actual.left;
-        actual.left = actual.right;
-        actual.right = aux;
-        reverse(actual.left);
-        reverse(actual.right);
     }
 
     private boolean verifyEquals(TreeNode left, TreeNode right) {
         if((left == null) != (right == null)) return false;
         if(left == null) return true;
         return left.val == right.val
-                && verifyEquals(left.left, right.left)
-                && verifyEquals(left.right, right.right);
+                && verifyEquals(left.right, right.left)
+                && verifyEquals(left.left, right.right);
     }
 }
