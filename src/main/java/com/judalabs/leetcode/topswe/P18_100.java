b5e1d4fa729bdb181eac98cca53c40b14c849c93
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-09-30 21:05:48 -0300

    problem 18 - is same tree

---
+++ src/main/java/com/judalabs/leetcode/topswe/P18_100.java
@@ -0,0 +1,17 @@
+package com.judalabs.leetcode.topswe;
+
+import com.judalabs.leetcode.topswe.helper.TreeNode;
+
+/*
+https://leetcode.com/problems/same-tree/
+ */
+public class P18_100 {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p != null && q == null || p == null && q != null) return false;
+        return p.val == q.val &&
+                isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right);
+    }
+}
--- src/test/java/com/judalabs/leetcode/topswe/P18_100Test.java
+++ src/test/java/com/judalabs/leetcode/topswe/P18_100Test.java
@@ -11,20 +11,20 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import com.judalabs.leetcode.topswe.helper.TreeNode;
 
-class P17_104Test {
-
+class P18_100Test {
 
     @ParameterizedTest
     @MethodSource("examples")
-    void tests(TreeNode input, int output) {
-        Assertions.assertEquals(output, new P17_104().maxDepth(input));
+    void tests(TreeNode p, TreeNode q, boolean result) {
+        Assertions.assertEquals(result, new P18_100().isSameTree(p, q));
     }
 
     public static Stream<Arguments> examples() {
         return Stream.of(
-                Arguments.of(arrayToTree(1), 1),
-                Arguments.of(arrayToTree(1,null, 2), 2),
-                Arguments.of(arrayToTree(3,9,20,null,null,15,7), 3)
+                Arguments.of(arrayToTree(1, 2, 1), arrayToTree(1, 1, 2), false),
+                Arguments.of(arrayToTree(1, 2, 3), arrayToTree(1, 2, 3), true),
+                Arguments.of(arrayToTree(1, 2), arrayToTree(1, null, 2), false),
+                Arguments.of(arrayToTree(1, null, 1), arrayToTree(1, 1, 1), false)
 
         );
     }
