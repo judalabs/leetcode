91e23d423449e4a164379367f3db010f58077e1d
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-10-02 22:52:10 -0300

    problem 18 - is same tree

--- src/main/java/com/judalabs/leetcode/topswe/P18_100.java
+++ src/main/java/com/judalabs/leetcode/topswe/P18_100.java
@@ -9,7 +9,7 @@ public class P18_100 {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if(p != null && q == null || p == null && q != null) return false;
+        if(p == null || q == null) return false;
         return p.val == q.val &&
                 isSameTree(p.left, q.left) &&
                 isSameTree(p.right, q.right);
