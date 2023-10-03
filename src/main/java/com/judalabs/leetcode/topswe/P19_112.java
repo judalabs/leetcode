43c807155a2b11dd99e9d0563fca9642f312f2ed
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-10-02 23:38:38 -0300

    problem 19 - path sum

---
+++ src/main/java/com/judalabs/leetcode/topswe/P19_112.java
@@ -0,0 +1,17 @@
+package com.judalabs.leetcode.topswe;
+
+import com.judalabs.leetcode.topswe.helper.TreeNode;
+
+/*
+https://leetcode.com/problems/path-sum/
+ */
+public class P19_112 {
+
+    public boolean hasPathSum(TreeNode actual, int decrease) {
+        if( actual == null ) return false;
+        decrease -= actual.val;
+
+        if(decrease == 0 && actual.left == null && actual.right == null) return true;
+        return hasPathSum(actual.left, decrease) || hasPathSum(actual.right, decrease);
+    }
+}
---
+++ src/test/java/com/judalabs/leetcode/topswe/P19_112Test.java
@@ -0,0 +1,30 @@
+package com.judalabs.leetcode.topswe;
+
+import static com.judalabs.leetcode.topswe.helper.LevelOrderTraversalHelper.arrayToTree;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.judalabs.leetcode.topswe.helper.TreeNode;
+
+class P19_112Test {
+
+    @ParameterizedTest
+    @MethodSource("examples")
+    void tests(TreeNode p, int targetSum, boolean result) {
+        Assertions.assertEquals(result, new P19_112().hasPathSum(p, targetSum));
+    }
+
+    public static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of(arrayToTree(1, 2), 1, false),
+                Arguments.of(arrayToTree(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1), 22, true),
+                Arguments.of(arrayToTree(), 0, false),
+                Arguments.of(arrayToTree(1, 2, 3), 5, false)
+        );
+    }
+}
