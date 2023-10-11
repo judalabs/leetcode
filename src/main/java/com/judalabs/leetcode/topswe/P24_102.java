09758b20848c7f20b2b39dc0ed2fbc326bf0b627
rodrigo Conceição <conceicaorodrigoj@gmail.com>
2023-10-11 15:01:46 -0300

    problem 24 - binary tree level order traversal

---
+++ src/main/java/com/judalabs/leetcode/topswe/P24_102.java
@@ -0,0 +1,48 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.judalabs.leetcode.topswe.helper.TreeNode;
+
+/*
+https://leetcode.com/problems/binary-tree-level-order-traversal/
+ */
+public class P24_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        Queue<Integer> level = new LinkedList<>();
+        final List<List<Integer>> lists = new ArrayList<>();
+        if(root == null) return new ArrayList<>();
+
+        queue.add(root);
+        lists.add(List.of(root.val));
+        level.add(0);
+        while (!queue.isEmpty()) {
+            final TreeNode actual = queue.remove();
+            final int thisLevel = level.remove();
+            final List<Integer> innerlist = new ArrayList<>();
+            if(actual.left != null) {
+                queue.add(actual.left);
+                innerlist.add(actual.left.val);
+                level.add(thisLevel + 1);
+
+            }
+            if(actual.right != null) {
+                queue.add(actual.right);
+                innerlist.add(actual.right.val);
+                level.add(thisLevel + 1);
+            }
+            if(!innerlist.isEmpty()) {
+                if(thisLevel + 1 < lists.size()) {
+                    lists.get(thisLevel + 1).addAll(innerlist);
+                } else {
+                    lists.add(innerlist);
+                }
+            }
+        }
+        return lists;
+    }
+}
---
+++ src/test/java/com/judalabs/leetcode/topswe/P24_102Test.java
@@ -0,0 +1,29 @@
+package com.judalabs.leetcode.topswe;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.judalabs.leetcode.topswe.helper.LevelOrderTraversalHelper;
+import com.judalabs.leetcode.topswe.helper.TreeNode;
+
+class P24_102Test {
+    @MethodSource("examples")
+    @ParameterizedTest
+    void tests(TreeNode input, List<List<Integer>> output) {
+        Assertions.assertEquals(output, new P24_102().levelOrder(input));
+    }
+
+    public static Stream<Arguments> examples() {
+        return Stream.of(
+                Arguments.of(LevelOrderTraversalHelper.arrayToTree(3,9,20,80,69,15,7),
+                        List.of(List.of(3), List.of(9, 20), List.of(80,69,15, 7))),
+                Arguments.of(LevelOrderTraversalHelper.arrayToTree(3, 9, 20, null, null, 15, 7),
+                        List.of(List.of(3), List.of(9, 20), List.of(15, 7)))
+        );
+    }
+}
